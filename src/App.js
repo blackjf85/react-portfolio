e045ed8068fdc648136bee515607bb8ee0884607
Jeremy <blackjf85@gmail.com>
2021-11-01 03:50:22 -0500

    App pulling in and displaying data from companion api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState("");
+  const [isTrue, setIsTrue] = useState(false);
+
+  const onClick = () => {
+    fetch("https://jfb-portfolio-api.herokuapp.com/projects")
+      .then((res) => res.json())
+      .then((response) => {
+        setData(response.data[0]);
+        console.log(response.data[0]);
+        setIsTrue(true);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Hello World</h1>
+      <button onClick={onClick}>Click Me</button>
+
+      {isTrue ? (
+        <div>
+          <h2>{data.title}</h2>
+          <p>{data.description}</p>
+          <img src={data.img_url} alt={data.title} />
+          <a href={data.repo} target="_blank" rel="noreferrer">
+            Source Code
+          </a>
+          <a href={data.demo} target="_blank" rel="noreferrer">
+            Demo
+          </a>
+        </div>
+      ) : (
+        <p>Press the button.</p>
+      )}
+
+      {/* <div>
+        <h2>{data.title}</h2>
+        <p>{data.description}</p>
+        <img src={data.img_url} alt={data.title} />
+        <a href={data.repo} target="_blank" rel="noreferrer">
+          Source Code
         </a>
-      </header>
+        <a href={data.demo} target="_blank" rel="noreferrer">
+          Demo
+        </a>
+      </div> */}
     </div>
   );
 }
